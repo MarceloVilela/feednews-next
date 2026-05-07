052cc9a9f8dbf07eb8866927954274f26f0b1b5f
Marcelo Vilela <marcelo.vilela.s@gmail.com>
2026-05-07 09:43:23 -0300

    refactor(api-sources): encode url, filename, classname

--- src/pages/api/game/sources/aHR0cHM6Ly9kcmFmdDUuZ2c=.ts
+++ src/pages/api/game/sources/aHR0cHM6Ly9kcmFmdDUuZ2c=.ts
@@ -35,32 +35,6 @@ class Dr4ft implements ISource {
 
     return { posts: [...postsData] };
   }
-
-  /*
-  async getHome_old(): Promise<IResponseHomeDTO> {
-    const url = this.getOriginUrl();
-
-    const { data } = await axios.get('https://api.draft5.gg/news/popular/day');
- 
-    const results = data.data.map((item: { postTitle: any; postSlug: any; postImage: any; }) => ({
-      link: `${url}/noticia/${item.postSlug}`,
-      title: item.postTitle,
-      thumb: `https://static.draft5.gg/${item.postImage}`,
-      created_at: '',
-    })).filter((elPost: any) => (elPost.thumb && elPost.title != "undefined"));
-
-    const { data: dataWeek } = await axios.get('https://api.draft5.gg/news/popular/day');
-
-    const resultsWeek = dataWeek.data.map((item: { postTitle: any; postSlug: any; postImage: any; }) => ({
-      link: `${url}/noticia/${item.postSlug}`,
-      title: item.postTitle,
-      thumb: `https://static.draft5.gg/${item.postImage}`,
-      created_at: '',
-    })).filter((elPost: any) => (elPost.thumb && elPost.title != "undefined"));  
-
-    return { posts: [...results, ...resultsWeek] };
-  }
-  */
 }
 
 export default new Dr4ft();
